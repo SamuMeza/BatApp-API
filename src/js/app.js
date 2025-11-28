c2396f6178cbac6a538574a646ee77d25745febe
Samuel <Samuel Meza>
2025-11-28 12:07:56 -0400

    iniciando proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -17,9 +17,33 @@ const contentContainer = document.getElementById("content-container")
 
 
 
-async function App () {
+const getDataCharacters = async () => {
   const data = await getData("https://api.batmanapi.com/v1/characters");
-  console.log(data);
+  data.forEach(element => {
+    const card = createCard(element);
+    contentContainer.appendChild(card);
+  });
 }
 
-App();
+const createCard = (data) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+    <img src="${data.image_url}" alt="${data.name}">
+    <h3>${data.name}</h3>
+    <p>Rol: ${data.rol}</p>
+    <p>Alias: ${data.alias}</p>
+    <p>Status: ${data.alive ? "Alive" : "Dead"}</p>
+    <p>Creado por: ${data.creator}</p>
+    <p>Descripción: ${data.description}</p>
+    <p>Primera aparición: ${data.first_appearance}</p>
+    <p>Género: ${data.gender}</p>
+    <ul>
+      ${data.abilities.map(ability => `<li>${ability}</li>`).join("")}
+    </ul>
+  `;
+
+  return card;
+}
+
+getDataCharacters();
